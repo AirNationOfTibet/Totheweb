826250c23c49fe9b255a1473ed762bd01a71312e
Tenzin Chosang <TenzinChosang@Tenzins-Air.Home>
2018-02-06 22:55:23 -0600

    changed server port

--- server/server1.js
+++ server/server1.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.use(express.static('server/public'));
-const port = 6000;
+const port = 4596;
 
 app.listen(port, function() {
   console.log(`server listening on port ${port}`);
